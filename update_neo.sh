132fa5be216f19c20781d8257b62ca4f0b10c565
MeFinity <72972952+MeFinity@users.noreply.github.com>
2023-11-18 04:06:29 +0700

    Update update_neo.sh

--- update_neo.sh
+++ update_neo.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-    echo "# Optional Package - Neocities
+echo "# Optional Package - Neocities
 # Repository: https://github.com/MeFinity/Neocities-Host
-198.51.233.100 neocities.org
-198.51.233.100 www.neocities.org
-198.51.233.100 blog.neocities.org" >> hosts
+$NEOCITIES_IP neocities.org
+$NEOCITIES_IP www.neocities.org
+$NEOCITIES_IP blog.neocities.org" > hosts
 
-current_page=0
+NEOCITIES_IP="198.51.233.100"
+USER_AGENT="Neocities Unblocker by KINI/1.3"
+concurrent_requests=20
 
-while :; do
-    current_page=$((current_page + 1))
-    echo -ne "Fetching pages... ($current_page)\r"
-
-    output=$(curl -s -H "Connection: keep-alive" -A "Neocities Unblocker by KINI/1.2" "https://neocities.org/browse?sort_by=last_updated&tag=&page=$current_page" | grep -Eo "https://[a-zA-Z0-9.-]+\.neocities\.org" | grep -v "www\.neocities\.org" | grep -v "blog\.neocities\.org")
+for ((current_page = 1; ; current_page++)); do
+    output=$(curl -s -H "Connection: keep-alive" -A "$USER_AGENT" "https://neocities.org/browse?sort_by=last_updated&tag=&page=$current_page" | grep -Eo "https://[a-zA-Z0-9.-]+\.neocities\.org" | grep -vE "www\.neocities\.org|blog\.neocities\.org")
 
     if [ -z "$output" ]; then
         break
     fi
 
-    echo "$output" | sed "s|^https://|198.51.233.100 |" | sort -u >> hosts
-    sleep $(echo "scale=2; $RANDOM / 32767" | bc)
+    echo -ne "..................($current_page)\r"
+
+    echo "$output" | sed "s|^https://|$NEOCITIES_IP |" | sort -u >> hosts &
+
+    if ((current_page % concurrent_requests == 0)); then
+        wait
+    fi
 done
+wait
 
 echo "Done fetching pages."
